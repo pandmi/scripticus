2582e4be68be48e104ce7552e15b3098ba47e5bc
Dmitry Pankov <dmpankov@gmail.com>
2020-08-03 15:49:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,51 @@
-from setuptools import setup
+import setuptools
 
-setup(
-  name='scripticus',
-  version='0.1.0',
-  author='Dmitry Pankov',
-  author_email='dpankov@mediamath.com',
-  packages=['scripticus'],
-  scripts=['scripticus/beautifulization','scripticus/looker_api','scripticus/mailicus','scripticus/t1_api'],
-  url='https://github.com/pandmi/scripticus/',
-  license='LICENSE.txt',
-  description='An absolutly awesome package that does everything!',
-  long_description=open('README.txt').read(),
-  install_requires=[
-  "pandas","numpy","seaborn","requests","os","ipywidgets",
-  "IPython.display","json",
-  "http","seaborn","matplotlib","csv","time","qds_sdk",
-  "sqlalchemy","datetime","pathlib","lxml","datetime","warnings",
-  "logging","requests","smtplib","ssl","os","email",
-  ],
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scripticus",
+    version="0.0.1",
+    author="Dmitry Pankov",
+    author_email="dp@pandmi.com",
+    description="An absolutly awesome package that does everything!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pandmi/scripticus",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
+
+
+
+
+
+
+# from setuptools import setup
+
+# setup(
+#   name='scripticus',
+#   version='0.1.0',
+#   author='Dmitry Pankov',
+#   author_email='dpankov@mediamath.com',
+#   packages=['scripticus'],
+#   scripts=['scripticus/beautifulization','scripticus/looker_api','scripticus/mailicus','scripticus/t1_api'],
+#   url='https://github.com/pandmi/scripticus/',
+#   license='LICENSE.txt',
+#   description='An absolutly awesome package that does everything!',
+#   long_description=open('README.txt').read(),
+#   install_requires=[
+#   "pandas","numpy","seaborn","requests","os","ipywidgets",
+#   "IPython.display","json",
+#   "http","seaborn","matplotlib","csv","time","qds_sdk",
+#   "sqlalchemy","datetime","pathlib","lxml","datetime","warnings",
+#   "logging","requests","smtplib","ssl","os","email",
+#   ],
+# )
 
 
 
