6707c3d004300f7b8237833ae6c23fe7f97aa92f
pandmi <dmpankov@gmail.com>
2020-08-31 17:21:36 +0200

    id change - f su

--- scripticus/t1_api.py
+++ scripticus/t1_api.py
@@ -581,7 +581,7 @@ class T1_API():
 
     def underpacing_strategies(self, campaign_ids, organization_id):
         strategy_ids, st_metadata_final = self.strategy_meta_data(campaign_ids)
-        df_deals = self.get_deals(campaign_ids)
+        df_deals = self.get_deals(strategy_ids)
         if len(df_deals) !=0:
             # df_deals = df_deals[['deal_id','deal_name','deal_identifier','deal_status','deal_floor_price','deal_creation_date']]
             df_deals = df_deals[['id','name','deal_identifier','status','price.value','created_on']]
