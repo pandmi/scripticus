cc1873e6ef7ca4eb5c8b9d2dcab50d96d2570139
pandmi <dmpankov@gmail.com>
2020-09-16 15:46:39 +0200

    adding reporticus beta

---
+++ scripticus/reporticus.py
@@ -0,0 +1,125 @@
+import requests  
+import pandas as pd
+import numpy as np
+from io import StringIO
+from io import BytesIO
+import io as stringIOModule
+import os
+import ipywidgets as widgets
+from ipywidgets import interact, interact_manual
+from IPython.display import display
+import json
+import http.client
+import seaborn as sns
+import matplotlib.pyplot as plt
+import csv
+import os
+import time
+import qds_sdk
+import sqlalchemy
+from qds_sdk.commands import *
+from matplotlib.ticker import FuncFormatter, MaxNLocator
+from matplotlib.text import Text
+import datetime
+import matplotlib.dates as mdates
+from pathlib import Path
+from lxml import etree
+import io
+from datetime import datetime, date, timedelta
+# import warnings
+# warnings.filterwarnings('ignore')
+
+def t1_api_login(username,password,client_id,client_secret):
+    response=requests.post('http://auth.mediamath.com/oauth/token',
+                                    data={'grant_type': 'http://auth0.com/oauth/grant-type/password-realm',
+                                            'username': username,
+                                            'password': password,
+                                            'client_id': client_id,
+                                            'client_secret': client_secret,
+                                            'realm': 'MediaMathActiveDirectory'
+                                            }) 
+    s = requests.session()
+    s.headers.update({'Accept': 'application/vnd.mediamath.v1+json','Authorization': 'Bearer {}'.format(response.json()['access_token'])})
+    resp = s.get('https://api.mediamath.com/api/v2.0/session')
+    return s
+
+class T1_API():
+
+    def __init__(self,username,password,client_id,client_secret):
+        self.username=username
+        self.password=password
+        self.client_id=client_id
+        self.client_secret=client_secret
+        self.session = t1_api_login(self.username,self.password, self.client_id,self.client_secret)
+        picard = 'https://api.mediamath.com/reporting/v1/std/'
+        r = self.session.get(picard + 'meta')
+        self.available_endpoints = list(json.loads(r.content)['reports'].keys())
+        self.available_endpoints.extend([
+            'performance_usd',
+            'performance_viewability',
+            'site_transparency_viewability',
+            'deals',
+            'performance_aggregated',
+            'performance_streaming'
+            ])
+        try:
+            assert endpoint in self.available_endpoints
+            self.endpoint = endpoint
+            if self.endpoint == 'deals': # This is while 'deals' is in beta
+                r = self.session.get('https://api.mediamath.com/reporting-beta/v1/std/deals/meta')
+            else:
+                r = self.session.get(picard + endpoint + '/meta')
+            self.info = json.loads(r.content)
+        except AssertionError:
+            raise AssertionError('{} is not a supported endpoint.  Endpoint must be one of the following: {}'.format(endpoint,self.available_endpoints))
+        except Exception as err:
+            print(r.content)
+            raise Exception(err)
+
+    def t1_report(self,*args,**kwargs): 
+    # defining parameters
+        self.params = {}
+        for k in args:
+            for v in k:
+                if type(k[v]) is list:
+                    self.params[v] = ','.join(k[v])
+                else:
+                    self.params[v] = k[v]
+        for k in kwargs:
+            if type(k) is list:
+                self.params[k] = ','.join(kwargs[k])
+            else:
+                self.params[k] = kwargs[k]
+    # creating a call
+        picard = 'https://api.mediamath.com/reporting/v1/std/'
+        all_reports_metadata = self.session.get(picard + 'meta')
+
+        if self.endpoint == 'transparency': 
+            self.endpoint = 'site_transparency'
+
+        if self.endpoint in (
+            'performance_usd',
+            'performance_viewability',
+            'site_transparency_viewability',
+            'performance_aggregated',
+            'performance_streaming'
+            ):
+            data_url = picard + self.endpoint
+        elif self.endpoint == 'deals':
+            data_url = 'https://api.mediamath.com/reporting-beta/v1/std/deals' 
+        else:
+            try:
+                report_metadata = all_reports_metadata.json()['reports'][self.endpoint]
+                try:
+                    data_url = report_metadata['URI_Data']
+                except KeyError:
+                    raise ValueError('No data URI found for API endpoint: {}'.format(self.endpoint))
+            except KeyError:
+                raise ValueError('No metadata found for API endpoint: {}'.format(self.endpoint))
+
+        self.response = self.session.get(data_url, params=self.params, headers={'Accept-Encoding':'identity','Connection':'close'})
+        data = response.content
+        df = pd.read_csv(BytesIO(data))
+        return df
+
+    # def pivot_report():
