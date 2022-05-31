03854c049f561ef0ee5fa4a25c9ffefe80a4ac9e
Dmitry Pankov <dp@pandmi.com>
2022-05-31 12:02:54 +0200

    fix number formating

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scripticus",
-    version="1.2.29",
+    version="1.2.30",
     author="Dmitry Pankov",
     author_email="dp@pandmi.com",
     description="An absolutly awesome package that does everything!",
