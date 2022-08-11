fb6cdf9f1b0cf4a28af18bd5b70de8d1c191ad81
Dmitry Pankov <dp@pandmi.com>
2022-08-11 12:19:34 +0200

    ver upd

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scripticus",
-    version="1.2.37",
+    version="1.2.39",
     author="Dmitry Pankov",
     author_email="dp@pandmi.com",
     description="An absolutly awesome package that does everything!",
