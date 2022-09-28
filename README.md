1bb89ef0f9169ab40ea5c4d30161558d0c7b8c95
Dmitry Pankov <dp@pandmi.com>
2022-09-28 17:35:44 +0200

    package doc update

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# scripticus
+
+### Scripticus
+
+A package for fast reporting, campaign monitoring and automation within T1 MediaMath ecosystem. 
+
+This package was created based on different APIs:  [MediaMath](https://apidocs.mediamath.com/), [Qubole](https://pypi.org/project/qds_sdk/) and [Looker](https://developers.looker.com/api/overview/).
+
+### Features
+
+- Access to all T1 platform reporting data and log-level marketing data from Qubole
+- Get bidding insights
+- Find under- and outpacing- strategies for further troubleshooting
+- Get recommendations for campaign optimization
+- Optimize campaign budgets, creatives, supply paths and more
+- Visualize all results via heatmaps, bar and line charts, etc.
+- Create and send a mail with findings and recommendations formatted as html or js - presentation file
+
+
+### Usage example
+
+```
+from scripticus import looker_api, bddr, t1_api as mm, beautifulization as bfz, mailicus as ms
+
+# Get recommendations for underpacing campaigns
+df_upc=t1_report.underpacing_campaigns(up_campaigns_ids, up_campaigns_to_check)  # returns a table with underpacing campaigns, their KPIs, and the most important settings. 
+bfz.up_campaign_table(df_upc) # returns a formatted table in which the settings to be changed are marked in correspomnding color.
+
+# Get recommendations for underpacing startegies
+df_ups=t1_report.underpacing_strategies(organization_id, campaign_active_up_ids)  # returns a table with underpacing startegies, their KPIs, and the most important settings. 
+bfz.up_strategy_table(df_ups)  # returns a formatted table in which the settings to be changed are marked in correspomnding color.
+```
--- setup.py
+++ setup.py
@@ -1,52 +1,51 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="scripticus",
-    version="1.2.45",
+    version="2.0.0",
     author="Dmitry Pankov",
     author_email="dp@pandmi.com",
-    description="An absolutly awesome package that does everything!",
+    description="A package for working with the MediaMath T1 ecosystem, creating reports, dashboards, and automating campaign optimization.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pandmi/scripticus",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
+    install_requires=[
+        "csv",
+        "datetime",
+        "email",
+        "http.client",
+        "io",
+        "IPython.display",
+        "ipywidgets",
+        "json",
+        "json",
+        "urllib.request",
+        "logging",
+        "lxml",
+        "matplotlib",
+        "numpy",
+        "os",
+        "pandas",
+        "pathlib",
+        "qds_sdk",
+        "re",
+        "requests",
+        "seaborn",
+        "smtplib",
+        "ssl",
+        "sqlalchemy",
+        "time",
+        "warnings"
+       ],
+    python_requires='>3.1.*',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: OS Independent"
     ],
 )
-
-
-
-
-
-
-# from setuptools import setup
-
-# setup(
-#   name='scripticus',
-#   version='0.1.0',
-#   author='Dmitry Pankov',
-#   author_email='dpankov@mediamath.com',
-#   packages=['scripticus'],
-#   scripts=['scripticus/beautifulization','scripticus/looker_api','scripticus/mailicus','scripticus/t1_api'],
-#   url='https://github.com/pandmi/scripticus/',
-#   license='LICENSE.txt',
-#   description='An absolutly awesome package that does everything!',
-#   long_description=open('README.txt').read(),
-#   install_requires=[
-#   "pandas","numpy","seaborn","requests","os","ipywidgets",
-#   "IPython.display","json",
-#   "http","seaborn","matplotlib","csv","time","qds_sdk",
-#   "sqlalchemy","datetime","pathlib","lxml","datetime","warnings",
-#   "logging","requests","smtplib","ssl","os","email",
-#   ],
-# )
-
-
-
 
