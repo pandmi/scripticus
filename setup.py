d8714737034c18558fcfd21e2adb216dc3bfe3a3
Dmitry Pankov <dp@pandmi.com>
2025-08-28 13:04:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="scripticus",
-    version="4.4.9",
+    version="4.5.0",
     author="Dmitry Pankov",
     author_email="dp@pandmi.com",
     description="A package for working with the MediaMath T1 ecosystem, creating reports, dashboards, and automating campaign optimization.",
@@ -25,6 +25,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],)
+
 
 
 
